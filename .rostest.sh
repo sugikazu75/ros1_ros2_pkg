1e584f1bddba7757f259195989560cc244afd5a7
sugihara <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2024-07-21 18:24:31 +0900

    add github action distro

--- .rostest.sh
+++ .rostest.sh
@@ -6,7 +6,13 @@ apt-get update -qq && apt-get install -y -q wget curl sudo lsb-release gnupg git
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 
 # Install ROS
-if [[ "$ROS_DISTRO" ==  "foxy" ]]; then # https://docs.ros.org/en/foxy/Installation/Ubuntu-Install-Debians.html
+if [[ "$ROS_DISTRO" ==  "noetic" ]]; then # https://wiki.ros.org/noetic/Installation/Ubuntu
+    sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main\" > /etc/apt/sources.list.d/ros-latest.list"
+    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+    sudo apt-get update -qq
+    sudo apt-get install -y -q ros-noetic-desktop-full
+    sudo apt-get install -y -q python3-catkin-pkg python3-catkin-tools python3-rosdep python3-wstool python3-rosinstall-generator python3-osrf-pycommon python-is-python3
+else # https://docs.ros.org/en/foxy/Installation/Ubuntu-Install-Debians.html
     locale  # check for UTF-8
     sudo apt-get update -qq  && sudo apt install -y -q locales
     sudo locale-gen en_US en_US.UTF-8
@@ -17,15 +23,8 @@ if [[ "$ROS_DISTRO" ==  "foxy" ]]; then # https://docs.ros.org/en/foxy/Installat
     sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
     sudo sh -c "echo \"deb [arch=`dpkg --print-architecture` signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu `. /etc/os-release && echo $UBUNTU_CODENAME` main\" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null"
     sudo apt-get update -qq
-    sudo apt-get install -y -q ros-foxy-desktop
+    sudo apt-get install -y -q ros-${ROS_DISTRO}-desktop
     sudo apt-get install -y -q python3-argcomplete python3-colcon-common-extensions python3-rosdep
-fi
-if [[ "$ROS_DISTRO" ==  "noetic" ]]; then # https://wiki.ros.org/noetic/Installation/Ubuntu
-    sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main\" > /etc/apt/sources.list.d/ros-latest.list"
-    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-    sudo apt-get update -qq
-    sudo apt-get install -y -q ros-noetic-desktop-full
-    sudo apt-get install -y -q python3-catkin-pkg python3-catkin-tools python3-rosdep python3-wstool python3-rosinstall-generator python3-osrf-pycommon python-is-python3
 fi
 
 source /opt/ros/${ROS_DISTRO}/setup.bash
